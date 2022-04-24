0c7899984fdf802a327f2ff5b7e0653178a4ee61
Zach Martin <zacharyadammartin@gmail.com>
2022-04-24 14:07:39 -0400

    Added matlab code to generate automated tests with noisy 16-QAM

--- QAM_demapper_tb.v
+++ QAM_demapper_tb.v
@@ -9,29 +9,33 @@
 module QAM_demapper_tb();
 endmodule
 
+
+/* Test the hard decision demapper datapath */
 module QAM_demapper_datapath_tb();
-	reg signed [7:0] I_in, Q_in;
+	reg signed [7:0] I_in, Q_in;	// Test input to the module
+	reg [7:0] expected_dout;
 	reg sclk, dclk, rst, en, cal;
 	
+	integer i;						// Loop index for accessing test words
+	reg [23:0] test_input [0:15]; 	// two test input words I_data_Q_data
+	
+	QAM_demapper_datapath UUT(.symbol_clock(sclk), .rst(rst), .I_in(I_in), .Q_in(Q_in)); // instantiate module
+	
+	// Access internal wires
 	wire [3:0] data_out;
-	integer i;
 	wire signed[7:0] i_internal;
 	wire signed [7:0] q_internal;
-	
-	reg [23:0] test_input [0:15]; // two test input words I_data_Q_data
-	reg [7:0] expected_dout;
-	QAM_demapper_datapath UUT(.symbol_clock(sclk), .rst(rst), .I_in(I_in), .Q_in(Q_in));
-	
 	assign data_out = UUT.val;
 	assign i_internal = UUT.I;
 	assign q_internal = UUT.Q;
+
 	
 	initial begin
 		rst <= 1;
 		I_in <= 0;
 		Q_in <= 0;
-		sclk <= 0;
-		dclk <= 0;
+		sclk <= 1;
+		dclk <= 1;
 		en <= 1; 
 		cal <= 0;
 		#10 begin 
@@ -40,17 +44,18 @@ module QAM_demapper_datapath_tb();
 		end
 	end
 	
-	// Create symbol clock reference
 	always
-		#10 sclk = ~sclk;
+		#10 sclk = ~sclk;	// symbol clock. Would normally come from a PLL
+	
+	always 
+		#5 dclk = ~dclk;	// internal data clock
 	
 	initial begin
 		$readmemh("S:\\projects\\QAM_demapper\\demapper_test_input.txt", test_input);
-
+		#20;	// Wait until reset sequence is done
 		for (i=0; i<16; i=i+1)begin
 			{I_in, Q_in, expected_dout} = test_input[i];
-			$display("I = %d  Q = %d i_internal = %d q_internal = %d  expected = %H  out = %H", I_in, Q_in, i_internal, q_internal, expected_dout, data_out);
-			#20;
+			#20 $display("I = %d  Q = %d i_internal = %d q_internal = %d  expected = %H  out = %H", I_in, Q_in, i_internal, q_internal, expected_dout, data_out);
 		end	
 	end
 	
